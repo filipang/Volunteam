9a342d9ec7a5451435b47a54f60e942d722ba3e2
filipang <33981940+filipang@users.noreply.github.com>
2019-06-03 00:35:29 +0300

    -merge?

--- app/src/main/java/com/volunteam/activities/MainActivity.java
+++ app/src/main/java/com/volunteam/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.volunteam.activities;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Handler;
 import android.os.Looper;
@@ -10,11 +11,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -22,13 +26,21 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.volunteam.R;
+import com.volunteam.components.MyAdapter;
 import com.volunteam.components.Voluntariat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     DrawerLayout drawer;
     ActionBarDrawerToggle toggle;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,27 +48,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //RECYCLERVIEW SETUP
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new MyAdapter(new ArrayList<Voluntariat>(Arrays.asList(Voluntariat.getTestList())));
+        recyclerView.setAdapter(mAdapter);
+
+
         //Spinner setup
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.main_spinner_list, R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        //!!!
+
         //Image loading is done on separate slide to avoid NetworkingOnMainThreadException
-        new Thread(new GetImageTask()).start();
+        //new Thread(new GetImageTask()).start();
 
         //NAV STUFF
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
-
         drawer = findViewById(R.id.drawer_layout);
-
         toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-
         NavigationView nav = findViewById(R.id.nav_view);
         nav.setNavigationItemSelectedListener(MainActivity.this);
     }
@@ -87,7 +107,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        Log.d("click", "AAAA" + menuItem.getItemId());
+        Log.d("click", "SOMETHING SOMETHING AT LEAST!???  " + menuItem.getOrder());
+        Intent intent;
+        switch (menuItem.getItemId()){
+            case R.id.menu_exploreaza:
+                break;
+            case R.id.menu_toate_vol:
+                intent = new Intent(this, ToateVolActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_vol_mele:
+                intent = new Intent(this, VolMeleActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_profil:
+                intent = new Intent(this, ProfilActivity.class);
+                startActivity(intent);
+                break;
+        }
         return true;
     }
 
@@ -101,6 +138,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     //!!!!
 
     //Task that loads Image solving threading/UI/Networking issues
+    /*
     class GetImageTask implements Runnable {
 
         private Handler handler;
@@ -149,5 +187,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
     }
+    */
 }
 
